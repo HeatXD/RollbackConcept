f8b9623fd6f10d917691ae4bcd6443838df5283f
HeatXD <45072324+HeatXD@users.noreply.github.com>
2020-09-22 19:36:36 +0200

    upd

--- tests.c
+++ tests.c
@@ -5,9 +5,12 @@
 int main(int argc, char const *argv[]) {
   PU_SESSION session;
   PU_PLAYER_TYPE player_type = PLAYER_CLIENT;
+
   pu_initialize(&session);
   ENetHost* client = pu_client_create(&session);
+
   pu_client_destroy(client);
   pu_deinitialize();
+
   return 0;
 }
